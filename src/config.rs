4eb094c9ed796785945d7128e09e513e3b9ee78b
João Figueira <business@jmc-figueira.dev>
2025-10-25 17:21:03 +0100

    Simplified the CLI interface and finished the deserialization logic for the config file

--- src/config.rs
+++ src/config.rs
@@ -1,6 +1,7 @@
-use std::{fs, path::PathBuf};
+use std::{collections::HashMap, fs, path::PathBuf};
 
-use clap::Parser;
+use bitflags::bitflags;
+use clap::{Parser, ValueHint, value_parser};
 use serde::Deserialize;
 use serenity::all::{ChannelId, GuildId};
 
@@ -9,63 +10,24 @@ use serenity::all::{ChannelId, GuildId};
 pub(crate) struct Args {
     #[arg(
         long = "config",
-        conflicts_with_all = ["server", "log_channel", "honeypot_channel", "idiot_proof"],
-        required_unless_present_any = ["server", "log_channel", "honeypot_channel", "idiot_proof"]
+        value_hint = ValueHint::FilePath,
+        default_value = "config.toml",
+        value_parser = value_parser!(PathBuf)
     )]
-    config_file: Option<PathBuf>,
-
-    #[arg(
-        long,
-        conflicts_with = "config_file",
-        required_unless_present = "config_file"
-    )]
-    server: Option<GuildId>,
-    #[arg(
-        long,
-        conflicts_with = "config_file",
-        required_unless_present = "config_file"
-    )]
-    log_channel: Option<ChannelId>,
-    #[arg(
-        long,
-        conflicts_with = "config_file",
-        required_unless_present = "config_file"
-    )]
-    honeypot_channel: Option<ChannelId>,
-    #[arg(
-        long = "tolerant",
-        short = 't',
-        conflicts_with = "config_file",
-        required_unless_present = "config_file"
-    )]
-    idiot_proof: bool,
+    config_file: PathBuf,
 }
 
 impl Args {
     pub(crate) fn to_config(&self) -> Config {
-        if let Some(ref cfg_path) = self.config_file {
-            match fs::read_to_string(cfg_path) {
-                Ok(cfg_str) => match toml::from_str(&cfg_str) {
-                    Ok(config) => config,
-                    Err(e) => {
-                        panic!("Error parsing the provided config file: {e}");
-                    }
-                },
+        match fs::read_to_string(&self.config_file) {
+            Ok(cfg_str) => match toml::from_str(&cfg_str) {
+                Ok(config) => config,
                 Err(e) => {
-                    panic!("Error reading the provided config file: {e}");
+                    panic!("Error parsing the provided config file: {e}");
                 }
-            }
-        } else {
-            let server_config = ServerConfig {
-                id: self.server.unwrap(),
-                log_channel: self.log_channel.unwrap(),
-                honeypot_channel: self.honeypot_channel.unwrap(),
-            };
-
-            Config {
-                server: vec![server_config],
-                default_actions: vec![ModerationActions::WarnMods], // TODO
-                tolerant: self.idiot_proof,
+            },
+            Err(e) => {
+                panic!("Error reading the provided config file: {e}");
             }
         }
     }
@@ -73,23 +35,31 @@ impl Args {
 
 #[derive(Deserialize)]
 pub(crate) struct Config {
-    server: Vec<ServerConfig>,
-    default_actions: Vec<ModerationActions>, // TODO: convert into bitflag
-    tolerant: bool,
+    servers: HashMap<GuildId, ServerConfig>,
 }
 
 #[derive(Deserialize)]
 struct ServerConfig {
-    id: GuildId,
     log_channel: ChannelId,
     honeypot_channel: ChannelId,
+    #[serde(default)]
+    mod_actions: ModerationActions,
+    tolerant: bool,
 }
 
-#[derive(Deserialize)]
-enum ModerationActions {
-    WarnMods,
-    EraseMessages,
-    Mute,
-    Kick,
-    Ban,
+bitflags! {
+    #[derive(Deserialize)]
+    struct ModerationActions: u8 {
+        const WarnMods = 0b00001;
+        const EraseMessages = 0b00010;
+        const Mute = 0b00100;
+        const Kick = 0b01000;
+        const Ban = 0b10000;
+    }
+}
+
+impl Default for ModerationActions {
+    fn default() -> Self {
+        Self::WarnMods | Self::EraseMessages | Self::Mute
+    }
 }
